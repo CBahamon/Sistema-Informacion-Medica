672fa0e679254060d4d053e027cc9ba3c72a9d50
Cristian Bahamon <bahamon2803@gmail.com>
2022-04-02 01:01:43 -0500

    Update Desing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaMedicosComponent } from './lista-medicos/lista-medicos.component';
 import { ListaPacientesComponent } from './lista-pacientes/lista-pacientes.component';
+import { RegistarPacienteComponent } from './registar-paciente/registar-paciente.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'pacientes',pathMatch:'full'},
   {path:'pacientes',component:ListaPacientesComponent},
-  {path:'', redirectTo:'pacientes',pathMatch:'full'}
+  {path:'medicos',component:ListaMedicosComponent},
+  {path:'registrar-paciente',component:RegistarPacienteComponent},
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPacientesComponent } from './lista-pacientes/lista-pacientes.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ListaMedicosComponent } from './lista-medicos/lista-medicos.component';
+import { RegistarPacienteComponent } from './registar-paciente/registar-paciente.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPacientesComponent
+    ListaPacientesComponent,
+    ListaMedicosComponent,
+    RegistarPacienteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
