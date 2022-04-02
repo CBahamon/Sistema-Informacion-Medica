0d365c1302fe20c8b8b1d7665207370af0513360
Cristian Bahamon <bahamon2803@gmail.com>
2022-04-02 00:58:35 -0500

    Save Persona

--- back-end/src/main/java/com/sistema/informacion/backend/controller/PersonaController.java
+++ back-end/src/main/java/com/sistema/informacion/backend/controller/PersonaController.java
@@ -3,10 +3,7 @@ package com.sistema.informacion.backend.controller;
 import com.sistema.informacion.backend.model.Persona;
 import com.sistema.informacion.backend.repository.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +15,26 @@ public class PersonaController {
     @Autowired
     private PersonaRepository repositorio;
 
+    //get all personas
     @GetMapping("/personas")
     public List<Persona> listAllPersonas(){
         return repositorio.findAll();
     }
+
+    //saved persona
+    @PostMapping("/personas")
+    public Persona savedPersona(@RequestBody Persona persona){
+        return repositorio.save(persona);
+    }
+
+//    @GetMapping("/pacientes")
+//    public List<Persona> listAllPacientes(@RequestParam int rol){
+//        try {
+//            return repositorio .
+//        }catch (Exception e){
+//
+//        }
+//        return repositorio.findByRol_persona(2L);
+//    }
 
 }
--- back-end/src/main/java/com/sistema/informacion/backend/repository/PersonaRepository.java
+++ back-end/src/main/java/com/sistema/informacion/backend/repository/PersonaRepository.java
@@ -2,8 +2,17 @@ package com.sistema.informacion.backend.repository;
 
 import com.sistema.informacion.backend.model.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
+
+//    @Query(
+//            value = "SELECT * FROM sistema_informacion.persona WHERE rol_persona = 3",
+//            nativeQuery = true
+//    )
+//    List<Persona> searchNativo(int rol);
 }
